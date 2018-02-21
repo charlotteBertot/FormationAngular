850cc59e717095c074e92d3c141988ab717658ff
charlotteBertot <charlotte.bertot@outlook.com>
2018-02-21 15:21:51 +0100

    create modules

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
+  imports: [
+    BrowserModule,
+    CoreModule,
+    HomeModule,
+    PageNotFoundModule,
+    SharedModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]

ec8854ecd8c97bfe780e744cf25a32bf5b677e76
charlotteBertot <charlotte.bertot@outlook.com>
2018-02-22 11:45:12 +0100

    suite

--- my-app/src/app/items/items.module.ts
+++ my-app/src/app/items/items.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListItemsComponent } from './containers/list-items/list-items.component';
 import { SharedModule } from '../shared/shared.module';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule
   ],
-  declarations: [ListItemsComponent],
+  declarations: [ListItemsComponent, ItemComponent],
   exports: [ListItemsComponent]
 })
 export class ItemsModule { }

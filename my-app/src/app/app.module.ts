0dac07918076dfb22240a6b0ad390ab616ea303e
charlotteBertot <charlotte.bertot@outlook.com>
2018-02-22 17:39:31 +0100

    dernier commit du jeudi

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from 'angularfire2';
 
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
@@ -11,6 +12,7 @@ import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { CollectionService } from './core/services/collection/collection.service';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment.prod';
 
 @NgModule({
   imports: [
@@ -20,7 +22,8 @@ import { AppRoutingModule } from './app-routing.module';
     HomeModule,
     NgbModule.forRoot(),
     SharedModule,
-    PageNotFoundModule
+    PageNotFoundModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   declarations: [
     AppComponent
